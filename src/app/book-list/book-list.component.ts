3d38b726d1fa967c5661c31e4b8f26920a4c90f9
eieik <eikhinezay378@gmail.com>
2021-01-07 14:13:32 +0630

    update

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-book-list',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListComponent implements OnInit {
 
-  constructor() { }
+  bookCount = 0;
+  bookList = [];
+  defaultImage = "assets/images/image_not_found.PNG";
+  
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.showBookCount();
   }
+
+  showBookCount(){
+     
+    const json = {
+     "categoryId": "CATEGORY10002",
+     "user_id": "USR2",
+     "author_id": "Author10001"
+     }
+     const header: HttpHeaders = new HttpHeaders({
+       token: "7584491bd16084688c1c1f74498177d9"
+     });
+     const url: string = "http://192.168.3.56:8080/elibrary" + "/home/book";
+     this.http.post(url, json, {
+      headers: header
+    }).subscribe(
+       (data: any) => {
+         console.warn("data: ", data);
+         this.bookCount = data.book_count;
+         this.bookList = data.latest_book;
+
+       },
+       error => {
+         console.warn("error: ", error);
+       });
+   }
+
+   
+
+   changeSource(event) { event.target.src = "assets/images/image_not_found.PNG" }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  categories = [];
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getCategories();
   }
 
+  getCategories(){
+    const header: HttpHeaders = new HttpHeaders({
+      token: "7584491bd16084688c1c1f74498177d9"
+    });
+    const url = "http://192.168.3.56:8080/elibrary" + "/category/all";
+    this.http.request('get',url, {
+      headers: header
+    }).subscribe(
+      (data: any) => {
+       this.categories = data.categories;
+       console.log(this.categories)
+
+      },
+      error => {
+        console.warn("error: ", error);
+      });
+  
+  }
 }
